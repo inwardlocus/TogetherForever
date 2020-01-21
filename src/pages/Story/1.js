c04c8e0e31a31c0487fcc2952788b3d0a115415d
japanvora <08me108@gmail.com>
2020-01-21 17:16:00 +0530

    Page  Addition

---
+++ src/pages/Story/1.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../../components/header"
+import Layout from "../../components/layout"
+
+export default () => (
+  <Layout>
+    <Header about="The One With The Blind Dates" />
+  </Layout>
+)
