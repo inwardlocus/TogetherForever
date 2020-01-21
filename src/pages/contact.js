4d3bd0dd2118ca29c635fa83c9c68211c298862e
japanvora <08me108@gmail.com>
2020-01-21 08:46:28 +0530

    Delete pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
 export default () => (
-  <Layout style={{ color: `green` }}>
+  <Layout>
     <Header about="Connect With Us" />
     <a href="mailto:08me108@gmail.com">Let's talk</a>
     <p>Drop us an email.</p>
