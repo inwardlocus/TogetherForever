3514efd7a8a2c327c451279112d44d2bd8535e84
japanvora <08me108@gmail.com>
2020-01-21 18:11:28 +0530

    css update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,9 @@ import Header from "../components/header"
 import Layout from "../components/layout"
 export default () => (
   <Layout>
-    <Header about="Connect With Us" />
+    <div style={{ textAlign: `center` }}>
+      <Header about="Connect With Us" />
+    </div>
     <a href="mailto:08me108@gmail.com">Let's talk</a>
     <p>Drop us an email.</p>
   </Layout>
