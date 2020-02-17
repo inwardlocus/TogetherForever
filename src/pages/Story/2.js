afd7592d64f4809856974cf1b4b721503b37796d
japanvora <08me108@gmail.com>
2020-02-17 19:22:57 +0530

    Sotry Image Addition

--- src/pages/Story/2.js
+++ src/pages/Story/2.js
@@ -1,15 +1,13 @@
 import React from "react"
 import Header from "../../components/header"
 import Layout from "../../components/layout"
+import two from "../../images/2.jpg"
 
 export default () => (
   <Layout>
     <div style={{ textAlign: `center` }}>
       <Header about="The One With The Tea Leaves" />
-      <img
-        src="https://i.pinimg.com/originals/4f/68/fe/4f68fe654645ea9e9f2374638b3e0daf.png"
-        alt="tf"
-      ></img>
+      <img src={two} alt="tf"></img>
     </div>
   </Layout>
 )
--- src/pages/Story/3.js
+++ src/pages/Story/3.js
@@ -1,15 +1,12 @@
 import React from "react"
 import Header from "../../components/header"
 import Layout from "../../components/layout"
-
+import three from "../../images/3.jpg"
 export default () => (
   <Layout>
     <div style={{ textAlign: `center` }}>
       <Header about="The One Where Everybody Finds Out" />
-      <img
-        src="https://i.pinimg.com/originals/4f/68/fe/4f68fe654645ea9e9f2374638b3e0daf.png"
-        alt="tf"
-      ></img>
+      <img src={three} alt="tf"></img>
     </div>
   </Layout>
 )
