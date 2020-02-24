7438cd9f7973cfecde0fe8a6a494d360405e0ffd
Ronak Baldha <5601641+ron4ex@users.noreply.github.com>
2020-02-24 17:22:10 +0530

    add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,23 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Together Forever`,
+        short_name: `TF`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/story/*`],
+      },
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-sharp`,
